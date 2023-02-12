e60906376bf043014bfd520a2603b389556d746a
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-02-12 17:20:43 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
 
+    #First method is responsible for setting a dogs name
+    # The name equals method takes in an argument of the dogs name and assigns that value to a variable
+    
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+
+    # This second method is responsible for getting a dogs name 
+
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+
+lassie.name = "Lassie"
+
+puts lassie.name
